f7d47afe61794fffd4b8a55c9d8aae9482b599fd
Kiyo-0 <sh.am432an@gmail.com>
2023-12-23 17:25:13 +0530

    Added dark theme table component to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimetabledarkComponent } from './components/timetabledark/timetabledark.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimetabledarkComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/timetabledark/timetabledark.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetabledark',
+  templateUrl: './timetabledark.component.html',
+  styleUrls: ['./timetabledark.component.css']
+})
+export class TimetabledarkComponent {
+
+}
